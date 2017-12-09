4814050886495121354c63bb7cc464db55c1f4a0
student <student@C5-D304-25.ad.utb.cz>
2017-12-09 13:01:39 +0100

    with recycler view

---
+++ app/src/main/java/cz/utb/fai/translator/api/pojo/ResponseTranslator.java
@@ -0,0 +1,21 @@
+package cz.utb.fai.translator.api.pojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ResponseTranslator {
+
+    @SerializedName("responseData")
+    private ResponseData responseData;
+
+    @SerializedName("responseStatus")
+    private int responseStatus;
+
+    public ResponseData getResponseData() {
+        return responseData;
+    }
+    public void setResponseData(ResponseData responseData) {
+        this.responseData = responseData;
+    }
+
+
+}
